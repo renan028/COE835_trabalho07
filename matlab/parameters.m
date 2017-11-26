d22aadaa5b845d92b0375408e1a7326cf84cdd59
renan028 <renan028@gmail.com>
2017-11-25 23:00:18 -0200

    ADD simulation ADD scripts like other projects

---
+++ matlab/parameters.m
@@ -0,0 +1,76 @@
+clear;
+clc;
+close all;
+global A w;
+
+PRINT = true;
+% PRINT = false;
+
+%Simulation time
+tfinal = 100;
+
+%Reference
+w = [1 3];
+a = [1 1];
+
+% Unit vectors
+e1 = [1 0]';
+e2 = [0 1]';
+
+% System matrix
+A = [0 1;0 0];
+
+%Monopoli multiplier
+Li = [1 1.5];
+
+%% First parameters
+
+kp_1 = 5;
+Z_1 = [1];
+P_1 = [1 2 1];
+
+k_1 = [1 1]';
+
+a_1 = [1 1];
+w_1 = [1 3];
+
+%Initial conditions
+y0_1  = [0 0]';
+theta0_1 = [0 0 0]';
+eta0_1 = [0 0]';
+lambda0_1 = [0 0]';
+rho0_1 = 0.5;
+
+%Adaptation gain
+Gamma_1 = eye(3);
+gamma_1 = 1;
+c1_1 = 1;
+c2_1 = 1;
+d1_1 = 1;
+d2_1 = 1;
+
+%% Second parameters
+
+kp_2 = 5;
+Z_2 = [1];
+P_2 = [1 2 1];
+
+k_2 = [1 1]';
+
+a_2 = [1 1];
+w_2 = [1 3];
+
+%Initial conditions
+y0_2  = [0 0]';
+theta0_2 = [1 1 1]';
+eta0_2 = [0 0]';
+lambda0_2 = [0 0]';
+rho0_2 = 0.5;
+
+%Adaptation gain
+Gamma_2 = eye(3);
+gamma_2 = 1;
+c1_2 = 1;
+c2_2 = 1;
+d1_2 = 1;
+d2_2 = 1;
--- matlab/simu_obs.m
+++ matlab/simu_obs.m
@@ -9,59 +9,224 @@
 %                   np = 4, 6     Adaptive parameters
 % Com observador completo
 %----------------------------------------------------------------------
-clear all
-clc
 
 global A B thetas A0 c1 c2 d1 d2 Gamma gamma kp a w e1 e2 k;
 
-kp = 5;
-Z = [1];
-P = [1 2 1];
+sim_str = strcat('sim0','_');
 
-thetas = [kp P(2) P(3)]';
+%% Simulation 1 (default)
+kp = kp_1;
+Z = Z_1;
+P = P_1;
+
+thetas_1 = [kp P(2) P(3)]';
 B = [0 kp]';
 
-e1 = [1 0]';
-e2 = [0 1]';
-k = [1 1]';
-A = [0 1;0 0];
+
+k = k_1;
 A0 = A - k*e1';
 
-a = [1 1];
-w = [1 3];
-c1 = 1;
-c2 = 1;
-d1 = 1;
-d2 = 1;
-Gamma = eye(3);
-gamma = 1;
-tfinal = 50;
+a = a_1;
+w = w_1;
+c1 = c1_1;
+c2 = c2_1;
+d1 = d1_1;
+d2 = d2_1;
+Gamma = Gamma_1;
+gamma = gamma_1;
 
 % Initialization
-y0  = [0 0]';
-theta0 = [0 0 0]';
-eta0 = [0 0]';
-lambda0 = [0 0]';
-rho0 = 0.5;
+y0  = y0_1;
+theta0 = theta0_1;
+eta0 = eta0_1;
+lambda0 = lambda0_1;
+rho0 = rho0_1;
 init = [y0' theta0' lambda0' eta0' rho0]';
 
-%% Plots
-[T,X] = ode23s('backstepping_obs',tfinal,init,'');
+[T_1,X_1] = ode23s('backstepping_obs',tfinal,init,'');
+y_1      = X_1(:,1);
+theta_1 =  X_1(:,3:5);
+tiltheta_1 = theta_1 - thetas';
+modtt_1 = sqrt(sum(theta_1.^2,2));
+r_1 = 0;
+for i=1:length(a)
+    r_1 = r_1 + a(i)*sin(w(i).*T_1);
+end
+e0_1 =  y_1 - r_1;
 
-y      = X(:,1);
 
-yr = a(1)*sin(w(1).*T) + a(2)*sin(w(2).*T);
-e =  y - yr;
+% %% Simulation 2 (gamma)
+% changed = 1;
+% 
+% kp = kp_1;
+% Z = Z_1;
+% P = P_1;
+% 
+% thetas = [kp P(2) P(3)]';
+% B = [0 kp]';
+% 
+% 
+% k = k_1;
+% A0 = A - k*e1';
+% 
+% a = a_1;
+% w = w_1;
+% c1 = c1_2;
+% c2 = c2_2;
+% d1 = d1_2;
+% d2 = d2_2;
+% Gamma = Gamma_2;
+% gamma = gamma_2;
+% 
+% % Initialization
+% y0  = y0_1;
+% theta0 = theta0_1;
+% eta0 = eta0_1;
+% lambda0 = lambda0_1;
+% rho0 = rho0_1;
+% init = [y0' theta0' lambda0' eta0' rho0]';
+% 
+% [T_2,X_2] = ode23s('backstepping_obs',tfinal,init,'');
+% y_2      = X_2(:,1);
+% theta_2 =  X_2(:,3:5);
+% tiltheta_2 = theta_2 - thetas';
+% modtt_2 = sqrt(sum(theta_2.^2,2));
+% 
+% r_2 = 0;
+% for i=1:length(a)
+%     r_2 = r_2 + a(i)*sin(w(i).*T_2);
+% end
+% e0_2 =  y_2 - r_2;
+% run plot_bkst.m;
 
-%Set matlab interpreter to latex
-set(groot, 'defaultAxesTickLabelInterpreter','latex');
-set(groot, 'defaultLegendInterpreter','latex');
-set(groot, 'defaultTextInterpreter','latex');
+% %% Simulation 3 (planta)
+% changed = 2;
+% 
+% kp = kp_2;
+% Z = Z_2;
+% P = P_2;
+% 
+% thetas_2 = [kp P(2) P(3)]';
+% B = [0 kp]';
+% 
+% 
+% k = k_1;
+% A0 = A - k*e1';
+% 
+% a = a_1;
+% w = w_1;
+% c1 = c1_1;
+% c2 = c2_1;
+% d1 = d1_1;
+% d2 = d2_1;
+% Gamma = Gamma_1;
+% gamma = gamma_1;
+% 
+% % Initialization
+% y0  = y0_1;
+% theta0 = theta0_1;
+% eta0 = eta0_1;
+% lambda0 = lambda0_1;
+% rho0 = rho0_1;
+% init = [y0' theta0' lambda0' eta0' rho0]';
+% 
+% [T_2,X_2] = ode23s('backstepping_obs',tfinal,init,'');
+% y_2      = X_2(:,1);
+% theta_2 =  X_2(:,3:5);
+% tiltheta_2 = theta_2 - thetas';
+% modtt_2 = sqrt(sum(theta_2.^2,2));
+% 
+% r_2 = 0;
+% for i=1:length(a)
+%     r_2 = r_2 + a(i)*sin(w(i).*T_2);
+% end
+% e0_2 =  y_2 - r_2;
+% run plot_bkst.m;
+% 
+% %% Simulation 4 (model)
+% changed = 3;
+% 
+% kp = kp_1;
+% Z = Z_1;
+% P = P_1;
+% 
+% thetas_2 = [kp P(2) P(3)]';
+% B = [0 kp]';
+% 
+% 
+% k = k_1;
+% A0 = A - k*e1';
+% 
+% a = a_2;
+% w = w_2;
+% c1 = c1_1;
+% c2 = c2_1;
+% d1 = d1_1;
+% d2 = d2_1;
+% Gamma = Gamma_1;
+% gamma = gamma_1;
+% 
+% % Initialization
+% y0  = y0_1;
+% theta0 = theta0_1;
+% eta0 = eta0_1;
+% lambda0 = lambda0_1;
+% rho0 = rho0_1;
+% init = [y0' theta0' lambda0' eta0' rho0]';
+% 
+% [T_2,X_2] = ode23s('backstepping_obs',tfinal,init,'');
+% y_2      = X_2(:,1);
+% theta_2 =  X_2(:,3:5);
+% tiltheta_2 = theta_2 - thetas';
+% modtt_2 = sqrt(sum(theta_2.^2,2));
+% 
+% r_2 = 0;
+% for i=1:length(a)
+%     r_2 = r_2 + a(i)*sin(w(i).*T_2);
+% end
+% e0_2 =  y_2 - r_2;
+% run plot_bkst.m;
 
-figure(1)
-clf
-plot(T,y,T,yr);grid;shg
-legend('y','yr','Location','SouthEast')
-title('$\epsilon$')
-print -depsc2 en3t0
-%---------------------------------------------------------------------
+%% Simulation 5 (y0)
+changed = 5;
+
+kp = kp_1;
+Z = Z_1;
+P = P_1;
+
+thetas_2 = [kp P(2) P(3)]';
+B = [0 kp]';
+
+
+k = k_1;
+A0 = A - k*e1';
+
+a = a_1;
+w = w_1;
+c1 = c1_1;
+c2 = c2_1;
+d1 = d1_1;
+d2 = d2_1;
+Gamma = Gamma_1;
+gamma = gamma_1;
+
+% Initialization
+y0  = y0_2;
+theta0 = theta0_2;
+eta0 = eta0_2;
+lambda0 = lambda0_2;
+rho0 = rho0_2;
+init = [y0' theta0' lambda0' eta0' rho0]';
+
+[T_2,X_2] = ode23s('backstepping_obs',tfinal,init,'');
+y_2      = X_2(:,1);
+theta_2 =  X_2(:,3:5);
+tiltheta_2 = theta_2 - thetas';
+modtt_2 = sqrt(sum(theta_2.^2,2));
+
+r_2 = 0;
+for i=1:length(a)
+    r_2 = r_2 + a(i)*sin(w(i).*T_2);
+end
+e0_2 =  y_2 - r_2;
+run plot_bkst.m;
